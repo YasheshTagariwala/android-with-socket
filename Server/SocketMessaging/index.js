27453f333941fc451729e6ae161dc37ed6dc2ac8
Yashesh Tagariwala <tagariwalayashesh@gmail.com>
2018-02-22 00:16:43 +0530

    One To One Messaging Working (I Think)

--- Server/SocketMessaging/index.js
+++ Server/SocketMessaging/index.js
@@ -14,45 +14,47 @@ app.get('/',function(req,res){
 })
 
 io.on('connection',function(socket){
-	allListeners(socket);
-	allEmitters(socket);
+	allListenersAndEmitters(socket);
 })
 
 
-function allListeners(socket){
+function allListenersAndEmitters(socket){
 
-	socket.on('privateMessage',function(data){	
-		var sockets = io.sockets.sockets;
-		socket.broadcast.emit('privateMessage',data);
+	socket.on('privateMessageEmit',function(data){
+		for(var i=0;i<users_list.length;i++){
+		    io.sockets.connected[users_list[i][JSON.parse(data).to].socket_id].emit('privateMessageGet',{"text":JSON.parse(data).message});
+//            socket.broadcast.to(users_list[i][JSON.parse(data).to].socket_id).emit('privateMessageGet',{"text":JSON.parse(data).message});
+		}
 	});
 
 	socket.on('connectedDone',function(data){
 		users_list.push(JSON.parse(data));
-		console.log("User added" + data);
 	});	
 
+    socket.on('groupMessage',function(data){
+//        var sockets = io.sockets.sockets;
+        socket.broadcast.emit('groupMessage',data);
+    });
 
 
 	socket.on('disconnect',function(){
-		users_list = users_list.filter(item => item.socket_id != socket.id);
-		console.log("user removed" + users_list);
+		users_list = users_list.filter(item => item.socket_id == socket.id);
+		console.log("user removed " + socket.id);
 	});
 
 	socket.on('heartbeat',function(data){
 		console.log("beating");
 	});
 
-}
-
-function allEmitters(socket){
 
 	socket.emit('connected',{"info":socket.id});
 	console.log("Yo Connected "+socket.id);
 
-	function sendHeartBeat(){		
+	function sendHeartBeat(){
 		setTimeout(sendHeartBeat,20000);
 	    socket.emit('ping', { beat : 1 });
 	}
 
-	setTimeout(sendHeartBeat,20000);	
+	setTimeout(sendHeartBeat,20000);
+
 }
