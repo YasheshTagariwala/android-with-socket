e39d773bd4a7d043d6affb9f3744f505f67f5a8f
Yashesh Tagariwala <tagariwalayashesh@gmail.com>
2018-02-24 00:17:25 +0530

    Solved The Message Queue Problem And Added PushNotification Emitter

--- Server/SocketMessaging/index.js
+++ Server/SocketMessaging/index.js
@@ -24,17 +24,20 @@ io.on('connect',function(socket){
 function allListenersAndEmitters(socket){
 
 	socket.on('privateMessageEmit',function(data){
-	    if(typeof users_list[JSON.parse(data).to] === 'undefined'){
+	    var data = JSON.parse(data);
+	    var user = users_list.filter(function(item){return item.email == data.to});
+	    if(user.length == 0){
 	        console.log("User Offline Message Queued");
 	    }else{
-	        io.sockets.connected[users_list[JSON.parse(data).to]].emit('privateMessageGet',{"text":JSON.parse(data).message});
+	        io.sockets.connected[user[0].socket_id].emit('privateMessageGet',{"text":data.message});
+	        io.sockets.connected[user[0].socket_id].emit("pushMessage",{"message":data.message,"doer":data.from});
 	    }
 
 	});
 
 	socket.on('connectedDone',function(data){
 	    var user = JSON.parse(data);
-	    users_list[user.email] = user.socket_id;
+        users_list.push(user);
 	    console.log(users_list);
 	});
 
@@ -44,8 +47,7 @@ function allListenersAndEmitters(socket){
 
 
 	socket.on('disconnect',function(){
-		var index = users_list.indexOf(socket.id);
-        if (index !== -1) users_list.splice(index, 1);
+        users_list = users_list.filter(function(item){return item.socket_id != socket.id});
 		console.log("user removed " + socket.id);
 	});
 
